ab62b23c5f1cd0d06d4ab638d5cba8b551b7754b
Saniya Khan <saniyakhanjlp@gmail.com>
2024-08-14 13:08:32 +0530

    fix all register controller bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,7 +18,7 @@ const registerUser = asyncHandler( async (req, res) => {
 
 
     const {fullName, email, username, password } = req.body
-    console.log("email: ", email);
+    //console.log("email: ", email);
 
     if (
         [fullName, email, username, password].some((field) => field?.trim() === "")
@@ -26,16 +26,24 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "Please fill in all fields")
     }
 
-   const existedUser =  User.findOne({
+   const existedUser =  await User.findOne({
         $or: [{ username } ,{ email }]
     })
 
     if (existedUser) {
         throw new ApiError(409, "Username or Email already exists")
     }
+    //console.log(req.files);
+    
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0 ){
+         coverImageLocalPath = req.files.coverImage[0].path
+    }
+
 
     if (!avatarLocalPath){
         throw new ApiError(400, "Please upload an avatar")
